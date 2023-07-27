1b40d38e1226f32b3c1f85610208b0974241b7a4
21ce025 <110348883+21ce025@users.noreply.github.com>
2023-07-27 13:24:46 +0530

    Add files via upload

---
+++ Practical 1-4/Practical 1.js
@@ -0,0 +1,17 @@
+const name="Unnati";
+const age=19;
+const height=5.0;
+const isBool=true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(isBool);
+
+function sum(a1,a2)
+{
+    return a1+a2;
+}
+
+ans=sum(5,15);
+console.log(ans);
